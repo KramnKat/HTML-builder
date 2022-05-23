da43ca3745023be75ad8a2f1211b09ceb61a4cff
KramnKat <kramnkaterinka@gmail.com>
2022-05-24 01:23:05 +0300

    feat: realize task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,33 @@
+const fs = require('fs')
+const path = require('path')
 
+const mainPath = path.resolve(__dirname, 'files')
+const mainPathCopy = path.join(__dirname, 'files-copy')
+
+
+fs.rm(mainPathCopy, {recursive: true, force: true}, () => {
+	fs.mkdir(mainPathCopy, {recursive: true}, (error) => {
+		if(error) {
+			console.log(error)
+		}
+	})
+	copyDirectory(mainPath, mainPathCopy)
+})
+
+function copyDirectory(fromPath, toPath) {
+	fs.readdir(mainPath, {withFileTypes: true}, (error, data) => {
+		if(error) {
+			console.log(error)
+		} else {
+			data.forEach(elem => {
+				if(elem.isDirectory()) {
+					fs.mkdir(path.resolve(toPath, elem.name), {recursive: true}, (error) => {
+						console.log(error)
+					})
+				} else {
+					fs.createReadStream(path.resolve(fromPath, elem.name), 'utf-8').pipe(fs.createWriteStream(path.resolve(toPath, elem.name)))
+				}
+			})
+		}
+	})
+}
