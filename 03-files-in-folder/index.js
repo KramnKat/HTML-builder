9d85920586933bec31f7e538fbff3bf124fb1767
KramnKat <kramnkaterinka@gmail.com>
2022-05-24 14:54:19 +0300

    refactor: fix bag in 03-foles-in-folder task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -5,19 +5,18 @@ const mainPath = path.resolve(__dirname, 'secret-folder')
 
 readdir(mainPath, {withFileTypes: true}, (error, data) => {
 	if (error) {
-		console.log(error)
-	} else {
-		data.forEach(el => {
-			if(el.isFile) {
+	console.log(error)
+	return
+	}
+		data.forEach((el) => {
+			if(el.isFile()) {
 				const [name, ext] = [path.parse(el.name).name, path.parse(el.name).ext]
 				stat(path.join(mainPath, el.name), (error, stats) => {
-					if (error) {
-						return console.log(error.message)
-					}
+					if (error) return console.log(error.message)
 					const size = stats.size
 					console.log(`${name} - ${ext.slice(1)} - ${(size / 1024).toFixed(1)}kb`)
 				})
 			}
 		})
 	}
-})
+)
